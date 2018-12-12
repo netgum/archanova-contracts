816f1271104ca5b55de405f44511fe57534b3755
Stanisław Głogowski <stanislaw.glogowski@gmail.com>
2018-12-12 20:25:18 +0100

    Updates migration

--- migrations/4_services.js
+++ migrations/4_services.js
@@ -1,8 +1,12 @@
 const BytesSignatureLibrary = artifacts.require('BytesSignatureLibrary');
 const AccountLibrary = artifacts.require('AccountLibrary');
 const AccountService = artifacts.require('AccountService');
+const AccountProxyService = artifacts.require('AccountProxyService');
 
 module.exports = async (deployer) => {
   deployer.link(BytesSignatureLibrary, AccountService);
   deployer.link(AccountLibrary, AccountService);
+
+  deployer.link(BytesSignatureLibrary, AccountProxyService);
+  deployer.link(AccountLibrary, AccountProxyService);
 };
