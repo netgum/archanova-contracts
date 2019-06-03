07b8c2c2bb8cb762425ba841d7ebd6c9ac3a0f77
Stanisław Głogowski <stanislaw.glogowski@gmail.com>
2019-06-03 18:15:52 +0200

    Fixes missing types

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,13 +1,16 @@
 export declare enum ContractNames {
-  Account = "Account",
-  AccountProvider = "AccountProvider",
-  AccountProxy = "AccountProxy",
-  ENSRegistry = "ENSRegistry",
-  ENSResolver = "ENSResolver",
-  Guardian = "Guardian",
-  VirtualPaymentManager = "VirtualPaymentManager",
+  Account = 'Account',
+  AccountFriendRecovery = 'AccountFriendRecovery',
+  AccountProvider = 'AccountProvider',
+  AccountProxy = 'AccountProxy',
+  ENSRegistry = 'ENSRegistry',
+  ENSResolver = 'ENSResolver',
+  Guardian = 'Guardian',
+  VirtualPaymentManager = 'VirtualPaymentManager',
 }
 
 export declare function getContractAddress(contractName: ContractNames, network: string): string;
+
 export declare function getContractAbi(contractName: ContractNames): any;
+
 export declare function getContractByteCodeHash(contractName: ContractNames): string;
