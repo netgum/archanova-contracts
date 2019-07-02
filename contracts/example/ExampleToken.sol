62fbdcf91006514ee19862a26bfc2760a462f989
Stanisław Głogowski <stanislaw.glogowski@gmail.com>
2019-07-02 11:41:24 +0200

    Adds example token

--- contracts/example/ExampleToken.sol
+++ contracts/example/ExampleToken.sol
@@ -1,12 +1,17 @@
 pragma solidity ^0.5.2;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 
 
 /**
  * @title Example Token
  */
-contract ExampleToken is ERC20 {
+contract ExampleToken is ERC20Detailed("ExampleToken", "ETK", 0), ERC20 {
+
+  constructor() public {
+    //
+  }
 
   function mint(uint256 amount) public returns (bool) {
     _mint(msg.sender, amount);
